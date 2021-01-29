184853c011de7e8b88cb32c256e3dd363eae3d2f
Rafael Costa Palma <rafael.palma@opah.com.br>
2021-01-29 11:32:17 -0300

    implementing no client message

--- src/components/Clients.tsx
+++ src/components/Clients.tsx
@@ -10,38 +10,50 @@ const TextLink = styled.a`
 
 const Clients: React.FC = () => {
   const {clients, removeClient} = useContext(GlobalContext);
-
-
   return (
     <>
       <div>Lista de Clientes:</div>
 
-      <table>
-        <tbody>
-          <tr>
-            <td>Nome</td>
-            <td>Editar</td>
-            <td>Excluir</td>
-          </tr>
-          {clients.map((client: any, i: any) => (
-            <tr key={client.id}>
-              <td>
-                {client.name} posicao {i}
-              </td>
-              <td>
-                <Link to={`/alterclient/${client.id}`}>Alterar Cliente</Link>
-              </td>
-              <td>
-                <TextLink
-                  onClick={() => removeClient(client.id)}
-                >
-                  Excluir Cliente
-                </TextLink>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      {clients.length > 0 ? (
+        <>
+          <table>
+            <tbody>
+              <tr>
+                <td>Nome</td>
+                <td>Editar</td>
+                <td>Excluir</td>
+              </tr>
+
+              <>
+                {clients.map((client: any, i: any) => (
+                  <tr key={client.id}>
+                    <td>
+                      {client.name} posicao {i}
+                    </td>
+                    <td>
+                      <Link to={`/alterclient/${client.id}`}>
+                        Alterar Cliente
+                      </Link>
+                    </td>
+                    <td>
+                      <TextLink
+                        onClick={() => removeClient(client.id)}
+                      >
+                        Excluir Cliente
+                      </TextLink>
+                    </td>
+                  </tr>
+                ))}
+              </>
+
+            </tbody>
+          </table>
+        </>
+      ) : (
+            <>
+              <p>NAO TEMOS CLIENTES PARA EXIBIR</p>
+            </>
+          )}
     </>
   );
 };
